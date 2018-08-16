1ab06f9a667c4be1fe43d0d1f9e4439dfffe5126
milrk丶钥匙 <37739619+zhazhazhazha@users.noreply.github.com>
2018-08-16 19:10:19 +0800

    1

--- ManageScoreDao/src/main/java/nb/stp/mansco/findpasswd/dao/FindPasswdDao.java
+++ ManageScoreDao/src/main/java/nb/stp/mansco/findpasswd/dao/FindPasswdDao.java
@@ -204,3 +204,58 @@ public class CartController extends BaseController{
 		return re;
 	}
 }
+public class UserBackManagerImpl extends GenericManagerImpl<UserBack, Long> implements UserBackManager {
+    @Override
+    public List<UserBack> findByCode(String postcode) {
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	@Column(name = "userName", nullable = false)
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	@Column(name = "userPassword", nullable = false)  
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	@Column(name = "userEmail") 
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+	@Column(name = "userPhone") 
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	
+	@Column(name = "userOrderAddress") 
+	public String getUserOrderAddress() {
+		return userOrderAddress;
+	}
+	public void setUserOrderAddress(String userOrderAddress) {
+		this.userOrderAddress = userOrderAddress;
+	}
+	
+	@Column(name = "createDate") 
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
