5abf000bcd8ed631ea5ccaa2c37379b5edc06062
莱月昂 <37739727+chengxuyuanya@users.noreply.github.com>
2018-08-27 13:48:24 +0800

    莱月昂

--- ManageScoreDao/src/main/java/nb/stp/mansco/userback/dao/UserBackDao.java
+++ ManageScoreDao/src/main/java/nb/stp/mansco/userback/dao/UserBackDao.java
@@ -1,8 +1,19 @@
-package nb.stp.mansco.userback.dao;
+package com.jack.dao;
 
+import java.util.List;
 
-import nb.stp.mansco.base.dao.GenericDao;
-import nb.stp.mansco.userback.domain.UserBack;
+import com.jack.entity.User;
+import com.util.dao.BaseDao;
+import com.util.entity.Pagination;
 
-public interface UserBackDao extends GenericDao<UserBack, Long> {
+public interface UserDao extends BaseDao<String, User>{
+
+	//可依据传递参数不同获取User对象集合
+	List<User> findUserList(User user);
+	
+	List<User> findAll();
+	
+	void deleteUser(User user);
+	
+	Pagination findUsers(Pagination pagination,User user);
 }
